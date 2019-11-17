6a777ae4d78f4c3847e0f5175362b86835b4c733
Alshohatee <46002084+Alshohatee@users.noreply.github.com>
2019-11-17 02:26:17 -0500

    made classes for the pokemon and trainter

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
+class Pokemon {
+  constructor(name){
+    this.name = name
+    this.hp = hp
+    this.atk = atk
+    this.abilities = []
+  }
+}
 
+
+class Trainer {
+  constructor(name) {
+    this.name = name
+    this.pokemonList = []
+
+  }
+  all (){
+  // this function will return an array of Pokemon objects
+  return this.pokemonList
+  }
+
+  get(name){
+    //this function will find the pokemon base on their name
+    // it will return them as objects to the arrayList
+    //API
+
+  }
+}
