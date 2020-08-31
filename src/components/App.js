2c466abae406e5cdd1475d941e9f31ed40caaf47
alshohatee <alshohateeaseel@gmail.com>
2020-08-31 16:31:55 -0400

    Fetch the pokemon api

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,40 @@
 import React from "react";
+import axios from "axios";
 import Header from "./Header";
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = [];
+    this.fetchPokemonApi = this.fetchPokemonApi.bind(this);
+  }
+  async fetchPokemonApi() {
+    try {
+      let response = await axios({
+        method: "get",
+        url: `https://pokeapi.co/api/v2/pokemon?limit=100&offset=200`,
+      });
+      this.setState({ data: response.data.results });
+    } catch (err) {
+      console.error(err);
+    }
+  }
+  componentDidMount() {
+    this.fetchPokemonApi();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        {this.state.data
+          ? this.state.data.map(function (item) {
+              return <p>{item.name}</p>;
+            })
+          : null}
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,16 +5,16 @@ function Header() {
     <div id="header">
       <h1>Pokedex</h1>
       <div id="nav">
-        <div class="navbar-item" onclick="goHome()">
+        <div className="navbar-item" onclick="goHome()">
           Home
         </div>
-        <div class="navbar-item" onclick="formTeam()">
+        <div className="navbar-item" onclick="formTeam()">
           Form a Team
         </div>
-        <div class="navbar-item" onclick="searchByName()">
+        <div className="navbar-item" onclick="searchByName()">
           Search for Pokemon by Name
         </div>
-        <div class="navbar-item" onclick="searchbyid()">
+        <div className="navbar-item" onclick="searchbyid()">
           Search for Pokemon by ID
         </div>
       </div>
