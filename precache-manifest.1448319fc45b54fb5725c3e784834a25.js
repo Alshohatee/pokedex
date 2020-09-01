939b49a7957c09984b5c3f724288f55911316c57
alshohatee <alshohateeaseel@gmail.com>
2020-09-01 11:05:36 -0400

    Updates

---
+++ precache-manifest.1448319fc45b54fb5725c3e784834a25.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cca81fe1635868f61a4d23576a124115",
+    "url": "/pokdex/index.html"
+  },
+  {
+    "revision": "5fbe08f726259aadbd26",
+    "url": "/pokdex/static/js/2.d38c6a3e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokdex/static/js/2.d38c6a3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83958a9abc33e10d9f2a",
+    "url": "/pokdex/static/js/main.8255ccce.chunk.js"
+  },
+  {
+    "revision": "d30e7ec24d81abeeec06",
+    "url": "/pokdex/static/js/runtime-main.9c180c2d.js"
+  }
+]);
