dbf4428b22bedf97a899a33399b6899e438f2ceb
alshohatee <alshohateeaseel@gmail.com>
2020-09-01 11:19:08 -0400

    Trying to deploy to github pages

---
+++ build/precache-manifest.efa334a1e0dcef949b5fba4efc99fe83.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d42e11c313480aa336e3056571ab5f73",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "5fbe08f726259aadbd26",
+    "url": "/pokedex/static/js/2.d38c6a3e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokedex/static/js/2.d38c6a3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83958a9abc33e10d9f2a",
+    "url": "/pokedex/static/js/main.8255ccce.chunk.js"
+  },
+  {
+    "revision": "5d5b7317cf43a0b8925c",
+    "url": "/pokedex/static/js/runtime-main.2d94522c.js"
+  }
+]);
